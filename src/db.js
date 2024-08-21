796ea34ae318c5abf183fbe114bae19879d894df
Gilberto Díaz <diazp.gilberto@gmail.com>
2024-08-20 19:36:22 -0500

    second commit

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const {Sequelize, DataTypes} = require("sequelize");
 require("dotenv").config()
 
 const {DB_USER,DB_PASSWORD,DB_HOST,DB_NAME} = process.env;
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@localhost:5432/voyant`)
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@localhost:5432/${DB_NAME}`)
 
 sequelize.define("User",{
     id:{
